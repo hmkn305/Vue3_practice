67d19874fe85ba6ed874b3876d707e3dad867e70
hmkn305 <hmkn.305@gmail.com>
2023-01-15 01:24:40 +0900

    追加分

--- chap03/computed/src/App.vue
+++ chap03/computed/src/App.vue
@@ -1,47 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, computed} from "vue";
+
+const radiusInit = Math.round(Math.random() * 10);
+const PI = ref(3.14);
+const radius = ref(radiusInit);
+const areaInit = PI.value * radiusInit * radiusInit;
+
+const area = computed(
+  (): number => {
+    return radius.value * radius.value * PI.value;
+  }
+);
+
+setInterval(
+  (): void => {
+    radius.value = Math.round(Math.random() * 10);
+  },
+  1000
+);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると{{ area }}となる。</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- chap03/reactive-function/src/App.vue
+++ chap03/reactive-function/src/App.vue
@@ -1,47 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {reactive, computed} from "vue";
+
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10)
+});
+
+const area = computed(
+  (): number => data.PI * data.radius * data.radius
+);
+
+setInterval(
+  (): void => {
+    data.radius = Math.round(Math.random() * 10);
+  },
+  1000
+);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると{{ area }}となる。</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
