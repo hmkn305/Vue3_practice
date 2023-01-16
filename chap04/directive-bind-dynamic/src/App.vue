76888c35da3751a553590cd8f0501e58e4d7836f
hmkn305 <hmkn.305@gmail.com>
2023-01-16 23:10:06 +0900

    ４章

---
+++ chap04/directive-bind-dynamic/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const widthOrHeight = ref("height");
+const height = ref(100);
+</script>
+
+<template>
+	<p><img alt="VueLogo" src="./assets/logo.svg" v-bind:[widthOrHeight]="height"></p>
+</template>
---
+++ chap04/directive-on-param-event/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lamg="ts">
+import {ref} from "vue";
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImageMousemove = (event: MouseEvent): void => {
+	mousePointerX.value = event.offsetX;
+	mousePointerY.value = event.offsetY;
+};
+</script>
+
+<template>
+	<section>
+		<img src="./assets/logo.svg" alt="vueのロゴ" width="200" height="200" v-on:mousemove="onImageMousemove">
+		<p>ポインタの位置: x={{mousePointerX}}; y={{mousePointerY}}</p>
+	</section>
+</template>
+
+<!-- 
+<script setup lang="ts">
+import {ref} from "vue";
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMousemove = (event: MouseEvent): void => {
+	mousePointerX.value = event.offsetX;
+	mousePointerY.value = event.offsetY;
+};
+</script>
+
+<template>
+	<section>
+		<img src="./assets/logo.svg" alt="Vueのロゴ" width="200" v-on:mousemove="onImgMousemove">
+		<p>ポインタの位置: x={{mousePointerX}}; y={{mousePointerY}}</p>
+	</section>
+</template> -->
